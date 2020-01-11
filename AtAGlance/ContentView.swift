778b2e2a37d8a8a80389f9e7b19d931f441b7338
DHKoch <dhktg2@mail.missouri.edu>
2020-01-10 21:48:38 -0600

    Initial Commit

---
+++ AtAGlance/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AtAGlance
+//
+//  Created by Dalton Koch on 1/10/20.
+//  Copyright © 2020 Dalton Koch. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
